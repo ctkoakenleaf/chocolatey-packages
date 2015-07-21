5470ca3591b2581641b10180960293cd5d55d1f1
Werner van Deventer <werner@brutaldev.com>
2015-07-21 23:40:13 +0200

    - Updates. - Removed Todoist which was actually Directory Monitor...

--- Todoist/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-$packageName = 'directorymonitor'
-$installerType = 'exe'
-$url = 'http://www.deventerprise.net/Download/DirectoryMonitor2/DirectoryMonitor2_Setup.exe'
-$url64 = $url
-$silentArgs = '/sp- /silent /norestart'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
