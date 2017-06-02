cfee8d40cbee85502ea49f104994859d83429ff3
Werner van Deventer <werner@brutaldev.com>
2017-06-02 21:25:43 +0200

    Updated MediaInfo to the latest version.

--- MediaInfo/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-$packageName = 'mediainfo'
-$installerType = 'exe'
-$url = 'http://mediaarea.net/download/binary/mediainfo-gui/0.7.70/MediaInfo_GUI_0.7.70_Windows.exe'
-$url64 = $url
-$silentArgs = '/S'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
---
+++ MediaInfo/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'mediainfo'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://mediaarea.net/download/binary/mediainfo-gui/0.7.96/MediaInfo_GUI_0.7.96_Windows.exe'
+$checksum = '9e390d003541a90d4e4d5f07a0ad3d02'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
